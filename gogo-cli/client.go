231bddbe7fd93c64983cbc72e38403b6ed6dcf9b
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-01-30 19:30:41 +0000

    Updated client.go

---
+++ gogo-cli/client.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+    "fmt"
+    "flag"
+    "strings"
+    "github.com/fmd/gogo/gogo-cli/protocols"
+)
+
+type Client struct {
+    Protocol protocols.IOProtocol
+}
+
+func (c *Client) parseProtocol(name string) {
+    p, err := protocols.GetProtocol(name)
+    if err != nil {
+        panic(err)
+    }
+
+    c.Protocol = p
+    fmt.Println(fmt.Sprintf("Using protocol: %s",c.Protocol.Flag()))
+}
+
+func (c *Client) parseVerbose(verbose bool) {
+
+}
+
+func (c *Client) Init() {
+    c.ParseFlags()
+}
+
+func (c *Client) ParseFlags() {
+    
+    //Protocol flag
+    firstFlag := protocols.GetProtocolFlags()[0]
+    pFlags := strings.Join(protocols.GetProtocolFlags(), ", ")
+    pMsg := fmt.Sprintf("I/O Protocol to use (%s)",pFlags)
+    var proto = flag.String("p", firstFlag, pMsg)
+    
+    //Verbose flag
+    var verbose = flag.Bool("v", false, "Verbose mode")
+
+    //Parse the flags
+    flag.Parse()
+
+    c.parseProtocol(*proto)
+    c.parseVerbose(*verbose)
+}
