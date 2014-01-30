6b81fa0ac7f61bbd7183f492b98a73934314dc69
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-01-30 15:06:14 +0000

    gofmt

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-    "github.com/codegangsta/martini"
-    "github.com/fmd/gogo/gogo"
+	"github.com/codegangsta/martini"
+	"github.com/fmd/gogo/gogo"
 )
 
 func main() {
-      m := martini.Classic()
-        m.Get("/", func() string {
-                return gogo.Board()
-                  })
-          m.Run()
+	m := martini.Classic()
+	m.Get("/", func() string {
+		return gogo.Board()
+	})
+	m.Run()
 }
