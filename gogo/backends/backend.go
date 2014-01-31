98dd30ed6c77c130f97dd68a65f986c0a04b39e0
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-01-31 09:51:03 +0000

    Added storage backends

---
+++ gogo/backends/backend.go
@@ -0,0 +1,37 @@
+package backends
+
+import (
+    "fmt"
+    "errors"
+)
+
+var (
+    //To add a new protocol, implement Backend and add its New() to this slice
+    Backends = []Backend{ NewFlatFile(), NewPostgres() }
+)
+
+type Backend interface {
+    Flag() string
+}
+
+func GetBackendFlags() []string {
+    flags := []string{}
+    for p := range Backends {
+        f := Backends[p]
+        flags = append(flags, f.Flag())
+    }
+
+    return flags
+}
+
+func GetBackend(name string) (Backend, error) {
+    for p := range Backends {
+        f := Backends[p]
+        if f.Flag() == name {
+            return f, nil
+        }
+    }
+    msg := fmt.Sprintf("backends: Could not find backend '%s'.", name)
+    err := errors.New(msg)
+    return nil, err
+}
---
+++ gogo/backends/postgres.go
@@ -0,0 +1,15 @@
+package backends
+
+type Postgres struct {}
+
+func NewPostgres() Backend {
+    //Create the object
+    p := &Postgres{}
+
+    //Return the object
+    return Backend(p)
+}
+
+func (p *Postgres) Flag() string {
+    return "postgres"
+}
