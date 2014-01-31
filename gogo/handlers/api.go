9b000864f44a7f37154b522152bf36fd690ed93f
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-01-31 19:09:00 +0000

    Updated api with session based auth.

--- gogo/handlers/api.go
+++ gogo/handlers/api.go
@@ -2,7 +2,7 @@ package handlers
 
 import (
 	//"fmt"
-	//"net/http"
+	"net/http"
 	"github.com/fmd/gogo/gogo/backends"
 	"github.com/fmd/gogo/gogo/protocols"
 	"github.com/codegangsta/martini"
@@ -49,17 +49,30 @@ func (u *MyUser) GetById(id interface{}) error {
 	return nil
 }
 
+func (a *ApiHandler) needsAuth(r render.Render, u sessionauth.User, req *http.Request) {
+	if u.IsAuthenticated() == false {
+		r.JSON(403,map[string]string{"status":"unauthorized"})
+		return
+	}
+}
 
 func (a *ApiHandler) loadRoutes() {
-	a.Martini.Get("/api/login", func(r render.Render) {
-		r.JSON(200,"Hello, world!")
+	a.Martini.Get("/api/login", func(s sessions.Session, u sessionauth.User, r render.Render) {
+		err := sessionauth.AuthenticateSession(s, u)
+		if err != nil {
+			r.JSON(500, "Internal server error")
+			return
+		}
+
+		r.JSON(200, u)
 	})
 
-	a.Martini.Get("/api/about", func(r render.Render) {
-		r.JSON(200,"About the world!")
+	a.Martini.Get("/api/logout", a.needsAuth, func(s sessions.Session, u sessionauth.User, r render.Render) {
+		sessionauth.Logout(s, u)
+		r.JSON(200, map[string]string{"status":"success"})
 	})
 
-	a.Martini.Get("/api/contact", func(r render.Render) {
+	a.Martini.Get("/api/game", a.needsAuth, func(r render.Render) {
 		r.JSON(200,"Cool the world!")
 	})
 
@@ -75,7 +88,7 @@ func NewApiHandler(m *martini.ClassicMartini, p protocols.IOProtocol, b backends
 	a.Martini = m
 	a.Store = sessions.NewCookieStore([]byte("secret123"))
 
-	a.Martini.Use(sessions.Sessions("go_session", a.Store))
+	a.Martini.Use(sessions.Sessions("gogo_session", a.Store))
 	a.Martini.Use(sessionauth.SessionUser(func () sessionauth.User {
 		return &MyUser{}
 	}))
