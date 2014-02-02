11e5f0c9a76a356937b39619fd87f3a2979b115e
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-02-02 13:00:30 +0000

    Updated main api

--- gogo/main.go
+++ gogo/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+    "strconv"
+    "net/http"
     //"github.com/eaigner/hood"
     "github.com/codegangsta/martini"
     "github.com/codegangsta/martini-contrib/render"
@@ -11,21 +13,40 @@ var b *Backend
 var s sessions.CookieStore
 var m *martini.ClassicMartini
 
-func routes(m *martini.ClassicMartini) {
-    m.Get("/api", func(r render.Render) {
-        r.JSON(200,"Cool the world!")
-    })
+func GetHomepage(req *http.Request, r render.Render) {
+    r.HTML(200,"index",nil)
+}
 
-    m.Get("/api/user/create", func(r render.Render) {
-        u := &User{}
-        u.Email = "fareeddudhia@gmail.com"
-        u.Username = "fmd"
-        err := b.Save(u)
-        if err != nil {
-            panic(err)
-        }
-        r.JSON(200,map[string]int{"id":int(u.Id)})
-    })
+func GetUser(req *http.Request, p martini.Params, r render.Render) {
+    id, err := strconv.Atoi(p["id"])
+
+    if err != nil {
+        r.JSON(500,"Internal server error")
+    }
+
+    var users []User
+    b.Hd.Where("id", "=", id).Limit(1).Find(&users)
+
+    if len(users) != 1 {
+        r.JSON(500,"Internal server error")   
+    }
+    
+    r.JSON(200,users[0])
+}
+
+func UpdateUser(req *http.Request, r render.Render) {
+
+}
+
+func CreateUser(req *http.Request, r render.Render) {
+    u := &User{}
+    u.Email = "fareeddudhia@gmail.com"
+    u.Username = "fmd"
+    err := b.Save(u)
+    if err != nil {
+        r.JSON(500,"Internal server error")
+    }
+    r.JSON(200,u)
 }
 
 func main() {
@@ -41,7 +62,10 @@ func main() {
     m.Use(sessions.Sessions("gogo_session", s))
     m.Use(render.Renderer())
 
-    routes(m)
+    m.Get(`/`, GetHomepage)
+    m.Post(`/api/user/create`, CreateUser)
+    m.Get(`/api/user/:id`, GetUser)
+    m.Post(`/api/user/:id/update`, UpdateUser)
 
     m.Run()
 }
